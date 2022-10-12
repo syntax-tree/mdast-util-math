acccebf376b4ff30fe55bb5dca8d613775752cf3
Titus Wormer <tituswormer@gmail.com>
2022-10-12 21:45:13 +0200

    Update dev-dependencies

--- complex-types.d.ts
+++ complex-types.d.ts
@@ -1,19 +1,23 @@
 import type {Literal} from 'mdast'
 
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 export interface Math extends Literal {
   type: 'math'
   meta?: string | null
 }
 
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 export interface InlineMath extends Literal {
   type: 'inlineMath'
 }
 
 declare module 'mdast' {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface StaticPhrasingContentMap {
     inlineMath: InlineMath
   }
 
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface BlockContentMap {
     math: Math
   }
