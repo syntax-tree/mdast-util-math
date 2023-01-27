c012b9a1b63986450f4884ea628c8e1c4ac57759
Titus Wormer <tituswormer@gmail.com>
2023-01-27 18:18:20 +0100

    Refactor types to use `CompileData` interface

--- complex-types.d.ts
+++ complex-types.d.ts
@@ -1,5 +1,13 @@
 import type {Literal} from 'mdast'
 
+declare module 'mdast-util-from-markdown' {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface CompileData {
+    // Register a new field.
+    mathFlowInside?: boolean | undefined
+  }
+}
+
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 export interface Math extends Literal {
   type: 'math'
